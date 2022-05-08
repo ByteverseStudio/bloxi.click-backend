c305e91ff642f0ddce8d306872a1a45e0da4d4aa
Julian Haag <me@jhaag.me>
2022-05-08 17:44:21 +0200

    Setup App

--- api/app.js
+++ api/app.js
@@ -1,6 +1,37 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+console.log('Starting app...');
+
 const app = express();
 
+app.use(express.json());
+
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/roblox-discord-sync';
+
+console.log('Connecting to mongodb...');
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+mongoose.Promise = global.Promise;
+
+//app.use
+
+//catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    res.status(404).json({ error: 'Not found' });
+}
+);
+
+//error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500).json({ error: err });
+}
+);
 
