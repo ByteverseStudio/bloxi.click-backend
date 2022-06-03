86fb3113706e05bae47eaf1b2c773530b7ce0b69
Julian Haag <me@jhaag.me>
2022-06-03 18:55:30 +0200

    Update Index Scripts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import { Hono } from 'hono'
+import { Context, Hono } from 'hono'
 import { poweredBy } from 'hono/powered-by'
 import { logger } from 'hono/logger'
+import { prettyJSON } from 'hono/pretty-json'
+import { cors } from 'hono/cors'
+import Toucan from 'toucan-js';
 
-export const app = new Hono()
+const app = new Hono()
 
 // Builtin middleware
 app.use('*', poweredBy())
 app.use('*', logger())
+app.use('*', cors())
+app.use('*', prettyJSON())
 
 // Add X-Response-Time header
 app.use('*', async (c, next) => {
@@ -18,18 +23,25 @@ app.use('*', async (c, next) => {
 
 // Custom Not Found Message
 app.notFound((c) => {
-  return c.text('Custom 404 Not Found', 404)
+  return c.json({ message: 'Not Found' }, 404)
 })
 
-// Error handling
-app.onError((err, c) => {
-  console.error(`${err}`)
-  return c.text('Custom Error Message', 500)
+//Error handling
+app.onError((err: Error, c: Context) => {
+  const sentry = new Toucan({
+    dsn: c.env.SENTRY_DSN,
+    context: c.event,
+    environment: c.env.ENVIRONMENT,
+  })
+  console.error(err)
+  const eventId = sentry.captureException(err)
+
+  return c.json({ message: 'Internal Server Error', eventId }, 500, { "X-Sentry-ID": eventId })
 })
 
-app.get('/', (c) => c.text('Hono!!'))
-
-app.get('/hello', () => new Response('This is /hello'))
+app.get('/', (c) => {
+  return c.json({ message: 'Hello World' })
+})
 
 
 export default app
